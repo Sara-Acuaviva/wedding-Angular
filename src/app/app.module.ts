0d6601b939ac3f0fe4a0d5194eb70a3baa21921e
Sara-Acuaviva <s.acuaviva@gmail.com>
2022-03-18 21:03:53 +0100

    se crean componentes básicos para aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TravelComponent } from './components/travel/travel.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { GiftListComponent } from './components/gift-list/gift-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TravelComponent,
+    FaqComponent,
+    GiftListComponent
   ],
   imports: [
     BrowserModule,
