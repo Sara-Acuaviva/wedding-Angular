b734eb66877c4d3168b1234b1e62a33215a2f532
Sara-Acuaviva <s.acuaviva@gmail.com>
2022-03-18 21:19:33 +0100

    Se agrega ruta a componente TRAVEL + ajuste selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TravelComponent } from './components/travel/travel.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "travel",
+    component: TravelComponent
   }
 ];
 
