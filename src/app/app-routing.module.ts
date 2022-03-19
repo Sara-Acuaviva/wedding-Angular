b8cfbb5d2e3bb0972862ce9fe7b2becaad7d9893
Sara-Acuaviva <s.acuaviva@gmail.com>
2022-03-19 20:21:50 +0100

    Creación de rutas y eliminación elemento main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './components/faq/faq.component';
+import { GiftListComponent } from './components/gift-list/gift-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { TravelComponent } from './components/travel/travel.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: "home",
     component: HomeComponent
   },
   {
     path: "travel",
     component: TravelComponent
+  },
+  {
+    path: "gift",
+    component: GiftListComponent
+  },
+  {
+    path: "faq",
+    component: FaqComponent
+  },
+  {
+    path: "**",
+    redirectTo: "home"
   }
+
 ];
 
 @NgModule({
