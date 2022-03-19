38ade53619163407e0100b576b9ce24f5c6c70f3
Sara-Acuaviva <s.acuaviva@gmail.com>
2022-03-19 20:43:45 +0100

    Se crea componente gift-list

--- src/app/components/gift-list/gift-list.component.ts
+++ src/app/components/gift-list/gift-list.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gift-list',
   templateUrl: './gift-list.component.html',
-  styleUrls: ['./gift-list.component.css']
+  styleUrls: ['./gift-list.component.css'],
 })
-export class GiftListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class GiftListComponent {
+  constructor() {}
 }
