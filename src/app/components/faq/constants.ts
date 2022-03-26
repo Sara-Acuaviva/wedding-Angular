8fd278548a7f4854db05e349573a55f671e37787
Sara-Acuaviva <s.acuaviva@gmail.com>
2022-03-26 20:21:55 +0100

    Se crea y usa archivo de constantes

---
+++ src/app/components/faq/constants.ts
@@ -0,0 +1,35 @@
+export const questionsList = [
+  {
+    title: '¿Cuánto tiempo antes debería llegar a Cádiz?',
+    description:
+      'Si viajas desde Colombia, lo ideal sería estar al menos un par de días antes del evento. Si el viaje es más corto, puedes llegar la noche antes sin problema.',
+  },
+  {
+    title: '¿Qué clima hace en Cádiz en esa época?',
+    description:
+      'En septiembre todavía es verano. Al tratarse de una ciudad costera, la temperatura es mucho más suave que en otras zonas del país. La Tª en esta época del año suele oscilar entre los 20/26º.',
+  },
+  {
+    title: '¿Cómo puedo ir hasta el lugar de la cena?',
+    description: ` Cádiz es una ciudad pequeña, tanto la ceremonia como la cena y la fiesta serán en el centro histórico de la
+      ciudad, así que puedes ir caminando o si prefieres puedes coger taxi. Si vas en coche puedes dejarlo en el parking del Hotel Atlántico (avísanos para reservarte la plaza!).`,
+  },
+  {
+    title: '¿Cuándo debería comprar los vuelos?',
+    description: `¡Cuanto antes mejor! Los precios suben a medida que se acerca la fecha, un buen precio para los vuelos desde
+      Colombia es de 500€ (ida y vuelta).`,
+  },
+  {
+    title: '¿Cuánto dura el viaje en tren desde Madrid?',
+    description: `El trayecto en tren desde Madrid a Cádiz suele durar entre 4h y 4h 30min (depende del tren que elijas).`,
+  },
+  {
+    title: '¿Cuánto dura el viaje en autobús desde Madrid?',
+    description: `En autobús desde Madrid hasta Cádiz se tardan unas 8 horas.`,
+  },
+  {
+    title: '¿Cuánto dura el viaje en avión desde Madrid?',
+    description: `En avión son tan sólo 1h hasta Jerez de la Frontera (XRY), desde ahí tendrás que coger tren para llegar a
+    Cádiz, y este trayecto dura aproximadamente 40 min.`,
+  },
+];
--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { questionsList } from './constants';
 
 @Component({
   selector: 'app-faq',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
-
+  Questions = questionsList; 
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
